da974101f181f19f10d5c30a3836873a5836efa5
dubewarsagar <30646108+dubewarsagar@users.noreply.github.com>
2022-10-10 11:12:27 +0530

    Added a code for amazon sigh in

--- src/test/java/com/browserstack/BStackDemoTest.java
+++ src/test/java/com/browserstack/BStackDemoTest.java
@@ -1,3 +1,4 @@
+/*
 package com.browserstack;
 
 import com.browserstack.BrowserStackRemoteTest;
@@ -27,5 +28,89 @@ public class BStackDemoTest extends BrowserStackRemoteTest {
         Assert.assertEquals(productOnScreenText, productOnCartText);
 
         System.out.println("Aaj hoil ka");
+    }
+}
+*/
+
+package amazonshopping.cart;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import http://amazon.in;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.google.common.base.Function;
+
+public class CrossBrowserScript {
+
+    WebDriver driver;
+
+    /**
+     * This function will execute before each Test tag in testng.xml
+     * @param browser
+     * @throws Exception
+     */
+    @BeforeTest
+    @Parameters("browser")
+    public void setup(String browser) throws Exception{
+        //Check if parameter passed from TestNG is 'firefox'
+        if(browser.equalsIgnoreCase("firefox")){
+        //create firefox instance
+            driver = new FirefoxDriver();
+        }
+        //Check if parameter passed as 'chrome'
+        else if(browser.equalsIgnoreCase("chrome")){
+            //set path to chromedriver.exe
+            System.setProperty("webdriver.chrome.driver",".\\chromedriver.exe");
+            //create chrome instance
+            driver = new ChromeDriver();
+        }
+        //Check if parameter passed as 'IE'
+                else if(browser.equalsIgnoreCase("ie")){
+                    //set path to IE.exe
+                    System.setProperty("webdriver.ie.driver",".\\IEDriverServer.exe");
+                    //create IE instance
+                    driver = new InternetExplorerDriver();
+                }
+        else{
+            //If no browser passed throw exception
+            throw new Exception("Browser is not correct");
+        }
+	baseUrl = "www.amazon.in";
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testParameterWithXML() throws InterruptedException{
+        driver.get("http://amazon.in");
+        //Find user name
+        WebElement userName = driver.findElement(By.id("ap_email"));
+        //Fill user name
+        userName.sendKeys("dubewar.sagar@gmail.com");
+        // Click Contienu
+        driver.findElement(By.id("contineu")).click();
+        //Find password
+        WebElement password = driver.findElement(By.id("ap_password"));
+        //Fill password
+        password.sendKeys("sagar@123");
+        // Click Contienu
+        driver.findElement(By.id("signInSubmit")).click();
+        
     }
 }
